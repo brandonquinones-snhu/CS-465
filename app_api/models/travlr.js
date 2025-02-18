60550e58baad5522b6c26eb1248ae8c75ec473f1
brandon quinones <brandon.quinones@snhu.edu>
2025-02-17 19:01:35 -0600

    Module 6 completed baseline

--- app_api/models/travlr.js
+++ app_api/models/travlr.js
@@ -10,6 +10,6 @@ const tripSchema = new mongoose.Schema({
     perPerson: { type: String, required: true },
     image: { type: String, required: true },
     description: { type: String, required: true }
-    });
-    const Trip = mongoose.model('trips', tripSchema);
-    module.exports = Trip;
+});
+const Trip = mongoose.model('trips', tripSchema);
+module.exports = Trip;
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+// This is where we import the controller we will route
 const tripsController = require("../controllers/trips");
 
-router.route("/trips").get(tripsController.tripsList);
-
+// define route for our trips endpoint
 router
+    .route("/trips")
+    .get(tripsController.tripsList) // Get Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+
+    // GET Method routes tripsFindByCode - requires parameter
+    // PUT Method routes tripsFindByCode - requires parameter
+    router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindbyCode);
+    .get(tripsController.tripsFindbyCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
